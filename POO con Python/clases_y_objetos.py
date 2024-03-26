d9e6d998981eeaa6298235390981b222f18b7071
AmilcarMflores <amilcarmflores@gmail.com>
2024-03-26 11:47:47 -0400

    POO con Python - Clases y Objetos

---
+++ POO con Python/clases_y_objetos.py
@@ -0,0 +1,20 @@
+#Comentarios:
+# separado_por_guiones_bajos --> Snake case
+# primerLetraEnMinusculaElRestoEnMayuscula --> Camel Case
+# PrimerLetraEnMinusculaElRestoEnMayuscula --> Pascal Case
+
+# La recomendación es usar pascal case
+class  Celular():
+  #Atributos estáticos
+  marca = "Samsung"
+  modelo = "S23"
+  camara = "48MP"
+
+#Instanciamos objetos  
+celular1 = Celular()
+celular2 = Celular()
+celular3 = Celular()
+celular4 = Celular()
+
+print(celular3)
+  
