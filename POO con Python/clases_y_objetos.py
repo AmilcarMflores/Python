dbf84dd4633f19caac7965ff6e2e4c2a277e15f7
AmilcarMflores <amilcarmflores@gmail.com>
2024-05-09 12:20:07 -0400

    Herencia

--- POO con Python/clases_y_objetos.py
+++ POO con Python/clases_y_objetos.py
@@ -4,17 +4,44 @@
 # PrimerLetraEnMinusculaElRestoEnMayuscula --> Pascal Case
 
 # La recomendación es usar pascal case
-class  Celular():
-  #Atributos estáticos
-  marca = "Samsung"
-  modelo = "S23"
-  camara = "48MP"
+# Crear clases y atributos: Se puede usar la misma forma de la POO en Java, pero en Python hay una forma más interesante:
 
-#Instanciamos objetos  
-celular1 = Celular()
-celular2 = Celular()
-celular3 = Celular()
-celular4 = Celular()
+# # En la forma tradicional (Java):
+# class  Celular():
+#   #Atributos estáticos
+#   marca = "Samsung"
+#   modelo = "S23"
+#   camara = "48MP"
 
-print(celular3)
+# #Instanciamos objetos  
+# celular1 = Celular()
+# celular2 = Celular()
+# celular3 = Celular()
+# celular4 = Celular()
+
+# print(celular3)
+
+# En Python:
+class Celular:
+  # Esta función se ejecuta automáticamente
+  # Es un método 'constructor'
+  # self --> uno mismo
+  def __init__(self, marca, modelo, camara):
+    self.marca = marca
+    self.modelo = modelo
+    self.camara = camara
   
+  # En un método siempre tenemos que mandar self para autoreferenciar así mismo
+  def llamar(self):
+    print(f'Estás haciendo un llamado desde un: {self.modelo}')
+    
+  def cortar(self):
+    print(f'Cortaste la llamada desde un: {self.modelo}')
+  
+celular1 = Celular("Samsung", "s23", "48mp")
+celular2 = Celular("Apple", "Iphone 15 pro", "96mp")
+
+#print(celular2.marca)
+
+celular1.llamar()
+
